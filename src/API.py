29e559e757a5094e785d7044ae2937c889975503
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2026-05-30 10:14:26 +0200

    added endpoint for filtered search

--- src/API.py
+++ src/API.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import uvicorn
 import time
@@ -8,12 +8,95 @@ app = FastAPI(root_path="/api/osumulti")
 db = DB()
 
 
+
+"""
+country:de,us,fr; public:true; diff:2.1-4.5; player:1-5; limit:10
+
+filter = {
+    "country": ["de", "us", "fr"],
+    "public": True,
+    "diff": [2.1, 4.5],
+    "player": [1, 5],
+    "limit": 10
+}
+"""
+
+
+@app.post("/filter_latest_lobbies")
+async def filter_latest_lobbies(request: Request):
+    search_filter = await request.json()
+    
+    data = db.get_latest_lobbies()
+    if data is None:
+        return JSONResponse(content={"message": "No data found"}, status_code=404)
+    
+    # validate filter
+    validated_filter = {}
+    for key, value in search_filter.items():
+        if key == "country":
+            if isinstance(value, list) and all(isinstance(v, str) for v in value):
+                validated_filter["country"] = [v.lower() for v in value]
+        elif key == "public":
+            if isinstance(value, bool):
+                validated_filter["public"] = value
+        elif key == "diff":
+            if isinstance(value, list) and len(value) == 2 and all(isinstance(v, (int, float, str)) for v in value):
+                validated_filter["diff"] = [float(v) for v in value]
+        elif key == "player":
+            if isinstance(value, list) and len(value) == 2 and all(isinstance(v, (int, float, str)) for v in value):
+                validated_filter["player"] = [int(v) for v in value]
+        elif key == "limit":
+            if isinstance(value, int) and value > 0:
+                validated_filter["limit"] = value
+                
+    # apply filter
+    def apply_filter(lobby:dict):
+        match = True
+        score = 0
+        for key, value in validated_filter.items():
+            if key == "country":
+                player_countries = [p["country_code"].lower() for p in lobby["recent_participants"]]
+                hits = sum(1 for pc in player_countries if pc in value)
+                if hits == 0:
+                    match = False
+                    break
+                score += hits
+            elif key == "public":
+                if lobby.get("has_password") == value:
+                    match = False
+                    break
+            elif key == "diff":
+                if not (value[0] <= lobby["difficulty_range"]["max"] and value[1] >= lobby["difficulty_range"]["min"]):
+                    match = False
+                    break
+            elif key == "player":
+                player_count = len(lobby["recent_participants"])
+                if not (value[0] <= player_count <= value[1]):
+                    match = False
+                    break
+                
+        return match, score, lobby
+    
+    filtered_lobbies = [apply_filter(lobby) for lobby in data["lobbies"]]
+    filtered_lobbies = [lobby for lobby in filtered_lobbies if lobby[0]] # filter matches
+    filtered_lobbies.sort(key=lambda x: x[1], reverse=True) # sort by score
+    filtered_lobbies = [lobby[2] for lobby in filtered_lobbies] # extract lobby data
+    
+    if "limit" in validated_filter:
+        filtered_lobbies = filtered_lobbies[:validated_filter["limit"]]
+    
+    data["lobbies"] = filtered_lobbies
+
+    return data
+
+
 @app.get("/latest_lobbies")
-def get_latest_lobbies():
+async def get_latest_lobbies():
     data = db.get_latest_lobbies()
     if data is None:
         return JSONResponse(content={"message": "No data found"}, status_code=404)
     return data
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
